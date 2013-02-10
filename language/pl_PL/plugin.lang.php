9f4789de77b350488ca19986bbc04e709eaaf4c1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-10 14:51:14 +0000

    [google2piwigo] Update pl_PL, thanks to : K.S.
    git-svn-id: http://piwigo.org/svn/extensions/Google2Piwigo@20646 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -21,14 +21,14 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['%d albums'] = '%d albumy';
-$lang['%d pictures are not displayed because already existing in the database.'] = '%d zdjęcia nie są wyświetlane ponieważ znajdu';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d zdjęcia nie są wyświetlane ponieważ znajdują się już w bazie';
 $lang['%d pictures imported'] = '%d zdjęć zaimportowanych';
 $lang['(%d photos)'] = '(%d zdjęć)';
 $lang['Begin transfer'] = 'Rozpocznij transfer';
 $lang['Fill these fields from Google datas'] = 'Wypełnij te pola danymi z Google';
 $lang['Import all my pictures'] = 'Importuj wszystkie moje zdjęcia';
 $lang['Import all photos in this album'] = 'Importuj wszystkie zdjęcia z tego albumu';
-$lang['Import options'] = 'Importuj opcje';
+$lang['Import options'] = 'Opcje importu';
 $lang['List my albums'] = 'Wyświetl albumy';
 $lang['List pictures of this album'] = 'Wyświetl zdjęcia z tego albumu';
 $lang['No download method available'] = 'Niedostępna żadna metoda pobierania';
@@ -37,4 +37,8 @@ $lang['Open Google page in a new tab'] = 'Otwórz Google w nowej zakładce';
 $lang['Processing...'] = 'Przetwarzanie...';
 $lang['Reproduce Google albums'] = 'Odbuduj albumy Google';
 $lang['Successfully logued to you Google account'] = 'Udane logowanie do Google';
+$lang['%d elements ready for importation'] = '%d elementów gotowych do importu';
+$lang['API not authenticated'] = 'API nieuwierzytelnione';
+$lang['Logged out'] = 'Wylogowany';
+$lang['Photo "%s" imported'] = 'Zdjęcie "%s" zaimportowane';
 ?>
