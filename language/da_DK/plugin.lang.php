365f2d72d820add14182a427844cc5cffac9448d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-10 19:56:38 +0000

    [google2piwigo] Add da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/Google2Piwigo@20661 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Successfully logued to you Google account'] = 'Det lykkedes at logge på din Google-konto';
+$lang['%d albums'] = '%d albummer';
+$lang['%d elements ready for importation'] = '%d elementer klar til import';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d billeder vises ikke fordi de allerede findes i databasen.';
+$lang['%d pictures imported'] = '%d billeder blev importeret';
+$lang['(%d photos)'] = '(%d fotografier)';
+$lang['API not authenticated'] = 'API ikke autoriseret';
+$lang['Begin transfer'] = 'Begynd overførsl';
+$lang['Fill these fields from Google datas'] = 'Udfyld disse felter fra Google-data';
+$lang['Import all my pictures'] = 'Importer alle mine billeder';
+$lang['Import all photos in this album'] = 'Importer alle fotografier i dette album';
+$lang['Import options'] = 'Importvalgmuligheder';
+$lang['List my albums'] = 'Liste over mine albummer';
+$lang['List pictures of this album'] = 'Liste over billeder i dette album';
+$lang['Logged out'] = 'Logget af';
+$lang['No download method available'] = 'Ingen downloadmetode er tilgængelig';
+$lang['One picture is not displayed because already existing in the database.'] = 'Et billede vises ikke fordi det allerede findes i databasen';
+$lang['Open Google page in a new tab'] = 'Åbn Google-side i et nyt faneblad';
+$lang['Photo "%s" imported'] = 'Fotografieret "%s" blev importeret';
+$lang['Processing...'] = 'Behandler ...';
+$lang['Reproduce Google albums'] = 'Reproducer Google-albummer';
+?>
