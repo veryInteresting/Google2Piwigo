f7b8223699e92b900cf169f003ece5743263955a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-15 16:34:40 +0000

    [google2piwigo] Add pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/Google2Piwigo@20766 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Processing...'] = 'Processando...';
+$lang['Reproduce Google albums'] = 'Reproduz álbuns do Google';
+$lang['Successfully logged to you Google account'] = 'Logado com  sucesso na sua conta do Google';
+$lang['%d albums'] = '%d álbuns';
+$lang['%d elements ready for importation'] = '%d elementos prontos para importação';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d fotos não mostradas pois já existem no banco de dados';
+$lang['%d pictures imported'] = '%d fotos importadas';
+$lang['(%d photos)'] = '%d fotos';
+$lang['API not authenticated'] = 'API não autenticada';
+$lang['Begin transfer'] = 'Iniciando transferência';
+$lang['Fill these fields from Google datas'] = 'Preencha esses campos com dados do Google';
+$lang['Geolocalization'] = 'Geolocalização';
+$lang['Import all my pictures'] = 'Importar todas as fotos';
+$lang['Import all photos in this album'] = 'Importar todas as fotos do álbum';
+$lang['Import options'] = 'Opções de importação';
+$lang['List my albums'] = 'Liste meus álbuns';
+$lang['List pictures of this album'] = 'Liste fotos desse álbum';
+$lang['Logged in as'] = 'Logado como';
+$lang['Logged out'] = 'Deslogar';
+$lang['No download method available'] = 'Nenhum médtodo de baixar fotos disponível';
+$lang['One picture is not displayed because already existing in the database.'] = 'Uma foto não mostrada porque já existe no banco de dados';
+$lang['Open Google page in a new tab'] = 'Abre página do google em outra aba';
+$lang['Photo "%s" imported'] = 'Foto %s importada';
+?>
