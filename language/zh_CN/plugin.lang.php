7112e78b04d2ccccb8b65a426c9573d518adc3d5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-19 07:51:33 +0000

    [google2piwigo] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/Google2Piwigo@20836 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Logged in as'] = '已以该身份登入';
+$lang['Processing...'] = '正在处理...';
+$lang['Reproduce Google albums'] = '复制 Google 相册';
+$lang['Successfully logged to you Google account'] = '已成功登入Google帐户';
+$lang['Fill these fields from Google datas'] = 'Fill these fields from Google datas';
+$lang['Logged out'] = '已登出';
+$lang['Open Google page in a new tab'] = '在新标签中打开Google页面';
+$lang['Photo "%s" imported'] = '图片 "%s" 已导入';
+$lang['%d albums'] = '%d 个相册';
+$lang['%d elements ready for importation'] = '%d 个元素准备导入';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d 张图片未显示，因为数据库中已存在。';
+$lang['%d pictures imported'] = '%d 张图片已导入';
+$lang['(%d photos)'] = '(%d 张图片)';
+$lang['API not authenticated'] = 'API未被认证';
+$lang['Begin transfer'] = '开始传输';
+$lang['Geolocalization'] = 'Geolocalization';
+$lang['Import all my pictures'] = '导入所有图片';
+$lang['Import all photos in this album'] = '导入此相册中的所有图片';
+$lang['Import options'] = '导入选项';
+$lang['List my albums'] = '列出我的相册';
+$lang['List pictures of this album'] = '列出此相册中的图片';
+$lang['No download method available'] = '没有可用的下载方法';
+$lang['One picture is not displayed because already existing in the database.'] = '有1张图片未显示，因为数据库中已存在。';
+?>
