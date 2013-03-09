436a3378904593f488d08a6b73fa6acb2bbee75d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-09 22:18:01 +0000

    [google2piwigo] Add bg_BG, thanks to : picsvet
    git-svn-id: http://piwigo.org/svn/extensions/Google2Piwigo@21423 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Reproduce Google albums'] = 'Възпроизвеждат Google албуми';
+$lang['Geolocalization'] = 'Geolocalization';
+$lang['Logged in as'] = 'Влезли сте като';
+$lang['API not authenticated'] = 'API които не са удостоверени';
+$lang['(%d photos)'] = '(%d снимки)';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d снимки не се показват, защото вече съществува в базата данни.';
+$lang['Open Google page in a new tab'] = 'Отворете Google страница в нов таб';
+$lang['One picture is not displayed because already existing in the database.'] = 'Една снимка не се показва, защото вече съществува в базата данни.';
+$lang['No download method available'] = 'Не е метод на изтегляне';
+$lang['Photo "%s" imported'] = 'Снимка"%s" внасят';
+$lang['Successfully logged to you Google account'] = 'Успешно сте влезли в профил в Google';
+$lang['Processing...'] = 'Обработва се ...';
+$lang['Logged out'] = 'Излязохте';
+$lang['List pictures of this album'] = 'Списък снимки на този албум';
+$lang['List my albums'] = 'List my albums';
+$lang['Import options'] = 'внос опции';
+$lang['Import all photos in this album'] = 'Импортиране на всички снимки в този албум';
+$lang['Import all my pictures'] = 'Импортиране на всички мои снимки';
+$lang['Fill these fields from Google datas'] = 'Попълнете тези полета от Google презареждане';
+$lang['Begin transfer'] = 'Започнете прехвърлянето';
+$lang['%d pictures imported'] = '%d внесени снимки';
+$lang['%d elements ready for importation'] = '%d елементи, готови за внасяне';
+$lang['%d albums'] = '%албуми';
+?>
