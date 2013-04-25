c90793b776fea4ac09f0bbff5a2d0fd5a5f92835
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-25 16:12:09 +0000

    [google2piwigo] Add hu_HU, thanks to : evryka23
    git-svn-id: http://piwigo.org/svn/extensions/Google2Piwigo@22361 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Successfully logged to you Google account'] = 'Sikeresen bejelentkezett a Google fiókba';
+$lang['Reproduce Google albums'] = 'Google albumok reprodukálása';
+$lang['Processing...'] = 'Feldolgozás alatt....';
+$lang['Photo "%s" imported'] = '"%s" kép importált';
+$lang['Open Google page in a new tab'] = 'Google oldal megnyítása egy külön fülön';
+$lang['One picture is not displayed because already existing in the database.'] = 'Egy kép nem jelenik meg, mert már létezik az adatbázisban.';
+$lang['No download method available'] = 'Nem áll rendelkezésre letöltési módszer';
+$lang['Logged out'] = 'Kijelentkezett';
+$lang['Logged in as'] = 'Belépve mint';
+$lang['List pictures of this album'] = 'Képek listázása az albumból';
+$lang['List my albums'] = 'Albumok listázása';
+$lang['Import options'] = 'Import lehetőségek';
+$lang['Import all photos in this album'] = 'Importálja az összes képem az albumból';
+$lang['Import all my pictures'] = 'Importálja az összes képem';
+$lang['Geolocalization'] = 'Geolokolizálás';
+$lang['Fill these fields from Google datas'] = 'Töltse ki ezeket a mezőket a Google adatokból';
+$lang['Begin transfer'] = 'Átvitel megkezdése';
+$lang['API not authenticated'] = 'API nem hitelesített';
+$lang['(%d photos)'] = '(%d képek)';
+$lang['%d pictures imported'] = '%d importált képek';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d a képek nem jelennek meg, mert már léteznek az adatbázisban.';
+$lang['%d elements ready for importation'] = '%d importálásra kész elemek';
+$lang['%d albums'] = '%d albumok';
+?>
