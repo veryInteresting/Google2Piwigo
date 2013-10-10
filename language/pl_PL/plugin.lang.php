c234832a2143bc2e3ae8cbf59087be17a53b5ec2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-10 04:19:05 +0000

    [google2piwigo] Update pl_PL, thanks to : K.S.
    git-svn-id: http://piwigo.org/svn/extensions/Google2Piwigo@24829 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -43,4 +43,5 @@ $lang['API not authenticated'] = 'API nieuwierzytelnione';
 $lang['Logged out'] = 'Wylogowany';
 $lang['Photo "%s" imported'] = 'Zdjęcie "%s" zaimportowane';
 $lang['Geolocalization'] = 'Geolokalizacja';
+$lang['Help! I get the error "The site [...] has not been registered."'] = 'Pomocy! Wyskakuje mi błąd "Strona [...] nie została zarejestrowana."';
 ?>
