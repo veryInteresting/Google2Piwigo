f0e083e533ede7803b1506133f04f68950a2a11b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-01 08:17:57 +0000

    [google2piwigo] Add zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/extensions/Google2Piwigo@25271 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fill these fields from Google datas'] = '用Google的資料，填寫這些欄位';
+$lang['Geolocalization'] = '定位技術';
+$lang['Photo "%s" imported'] = '圖片 "%s" 已導入';
+$lang['Processing...'] = '正在處理...';
+$lang['Reproduce Google albums'] = '複製 Google 相冊';
+$lang['Successfully logged to you Google account'] = '已成功登入Google帳戶';
+$lang['%d albums'] = '%d 個相冊';
+$lang['%d elements ready for importation'] = '%d 個元素準備導入';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d 張圖片未顯示，因為數據庫中已存在。';
+$lang['%d pictures imported'] = '%d 張圖片已導入';
+$lang['(%d photos)'] = '(%d 張圖片)';
+$lang['API not authenticated'] = 'API未被認證';
+$lang['Begin transfer'] = '開始傳輸';
+$lang['Help! I get the error "The site [...] has not been registered."'] = '請幫忙！我遇到了一個錯誤「網站[...]並沒有註冊。」';
+$lang['Import all my pictures'] = '導入所有圖片';
+$lang['Import all photos in this album'] = '導入此相冊中的所有圖片';
+$lang['Import options'] = '導入選項';
+$lang['List my albums'] = '列出我的相冊';
+$lang['List pictures of this album'] = '列出此相冊中的圖片';
+$lang['Logged in as'] = '已以該身份登入';
+$lang['Logged out'] = '已登出';
+$lang['No download method available'] = '沒有可用的下載方法';
+$lang['One picture is not displayed because already existing in the database.'] = '有1張圖片未顯示，因為數據庫中已存在。';
+$lang['Open Google page in a new tab'] = '在新標籤中打開Google頁面';
+?>
