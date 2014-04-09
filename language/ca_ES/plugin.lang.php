1dc206f73436174dcc1f5e3b6314e160759b55d7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-09 17:29:13 +0000

    [google2piwigo] Add ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/extensions/Google2Piwigo@28138 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Reproduce Google albums'] = 'Reprodueix els àlbums de Google';
+$lang['Open Google page in a new tab'] = 'Obre la pàgina de Google en una nova pestanya';
+$lang['Photo "%s" imported'] = 'S\'ha importat la foto "%s" ';
+$lang['Processing...'] = 'S\'està processant...';
+$lang['%d albums'] = '%d àlbums';
+$lang['%d elements ready for importation'] = '%d elements preparats per a ser importats';
+$lang['%d pictures imported'] = 'S\'han importat %d imatges';
+$lang['(%d photos)'] = '(%d fotos)';
+$lang['API not authenticated'] = 'API no autenticada';
+$lang['Begin transfer'] = 'Comença la transferència';
+$lang['Geolocalization'] = 'Geolocalització';
+$lang['Import all my pictures'] = 'Importa totes les meves imatges';
+$lang['Import all photos in this album'] = 'Importa totes les fotos d\'aquest àlbum';
+$lang['Import options'] = 'Opcions d\'importació';
+$lang['List my albums'] = 'Llista els meus àlbums';
+$lang['List pictures of this album'] = 'Llista les imatges d\'aquest àlbum';
+$lang['No download method available'] = 'No hi ha disponible cap mètode de descàrrega';
