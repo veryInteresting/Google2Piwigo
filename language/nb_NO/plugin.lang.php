68de8522b57e2898c585c80e536e0db379d3dfad
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-24 17:08:17 +0000

    [google2piwigo] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/Google2Piwigo@30297 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Processing...'] = 'Behandler...';
+$lang['Reproduce Google albums'] = 'Reprodusere Google album';
+$lang['Successfully logged to you Google account'] = 'Du er nå logget på din Google-konto';
+$lang['Please fill your API keys on the configuration tab'] = 'Vennligst fyll ut din API nøkkel i konfigurasjons fanen';
+$lang['Photo "%s" imported'] = 'Bilde "%s" importert';
+$lang['Open Google page in a new tab'] = 'Åpne Google siden i en ny fane';
+$lang['One picture is not displayed because already existing in the database.'] = 'Et bilde vises ikke pga det allerede finnes i databasen';
+$lang['List my albums'] = 'Sortere mine albumer';
+$lang['No download method available'] = 'Ingen nedlastnings metode er tilgjengelig';
+$lang['Logged out'] = 'Logget ut';
+$lang['Logged in as'] = 'Logget på som';
+$lang['List pictures of this album'] = 'Sortere bildene i denne albumen';
+$lang['Import all photos in this album'] = 'Importer alle bildene i denne album';
+$lang['Import options'] = 'Importer valg';
+$lang['Import all my pictures'] = 'Importer alle bildene mine';
+$lang['How do I get my Google project API key?'] = 'Hvordan får jeg ta i min Google prosjekt API nøkkel?';
+$lang['Google project keys'] = 'Google prosjekt nøkkel';
+$lang['Geolocalization'] = 'Geolokalisering';
+$lang['Fill these fields from Google datas'] = 'Fyll disse feltene med Google data';
+$lang['Callback URL'] = 'Kall tilbake URL';
+$lang['Begin transfer'] = 'Begynn overføring';
+$lang['API not authenticated'] = 'API ikke autentisert';
+$lang['(%d photos)'] = '(%d bilder)';
+$lang['%d pictures imported'] = '%d bilder er importert';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d bilder vises ikke fordi de allerede eksisterer i databasen.';
+$lang['%d elements ready for importation'] = '%d elementer klar for innførsel';
+$lang['%d albums'] = '%d albumer';
